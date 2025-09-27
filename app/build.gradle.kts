a9a430db98706a2c987647b18efee0da394eb24b
Tawanda Brandon <work@tawandabrandon.co>
2025-09-28 01:05:54 +0200

    Update SDK versions and library dependencies: downgrade compileSdk and targetSdk to 33, and adjust coreKtx, lifecycleRuntimeKtx, and composeBom versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.chibatamoto"
-    compileSdk = 35
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.chibatamoto"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
